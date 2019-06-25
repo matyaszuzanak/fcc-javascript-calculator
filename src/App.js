411741ca7abc8a3c32c4499305c26d8ab8a89e70
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-25 19:18:35 +0200

    enter

--- src/App.js
+++ src/App.js
@@ -2,25 +2,69 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var oneBtn = document.getElementById("calc-one");
+var twoBtn = document.getElementById("calc-two");
+var threeBtn = document.getElementById("calc-three");
+var fourBtn = document.getElementById("calc-four");
+var fiveBtn = document.getElementById("calc-five");
+var sixBtn = document.getElementById("calc-six");
+var sevenBtn = document.getElementById("calc-seven");
+var eightBtn = document.getElementById("calc-eight");
+var nineBtn = document.getElementById("calc-nine");
+var zeroBtn = document.getElementById("calc-zero");
+
+var decimalBtn = document.getElementById("calc-decimal");
+var clearBtn = document.getElementById("calc-clear");
+var backspaceBtn = document.getElementById("calc-backspace");
+var displayValElement = document.getElementById("calc-display-val");
+
+var displayVal = "0";
+var pendingVal;
+var evalStringArray = [];
+
+var calcNumBtns = document.getElementsByClassName("calc-btn-num");
+var calcOperatorBtns = document.getElementsByClassName("calc-btn-operator");
+
+var updateDisplayVal = (clickObj) => {
+  var btnText = clickObj.target.innerText;
+
+  if (displayVal === "0")
+    displayVal = "";
+  displayVal += btnText;
+  displayValElement.innerText = displayVal;
+}
+
+for (let i = 0; i < calcNumBtns.length; i++) {
+  calcNumBtns[i].addEventListener("click", updateDisplayVal, false);
+}
+//for (let i = 0; i < calcOperatorBtns.length; i++) {
+//  calcOperatorBtns[i].addEventListener("click", performOperation, false);
+//} 
+
+clearBtn.onclick = () => {
+  displayVal = "0";
+  pendingVal = undefined;
+  evalStringArray = [];
+  displayValElement.innerHTML = displayVal;
+}
+
+backspaceBtn.onclick = () => {
+  let lengthOfDisplayVal = displayVal.length;
+  displayVal = displayVal.slice(0, lengthOfDisplayVal - 1);
+
+    if(displayVal === "")
+      displayVal = "0";
+
+    displayValElement.innerText = displayVal;
+}
+
+decimalBtn.onclick = () => {
+  if(!displayVal.includes("."))
+    displayVal += ".";
+  displayValElement.innerText = displayVal;
+}
+
+function App() { 
 }
 
 export default App;
